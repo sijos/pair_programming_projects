1bbac851e9a665acaa804655d726c7803cf8304d
sijos <ssny88@gmail.com>
2017-02-04 12:08:41 -0800

    convert tabs to spaces for github viewing

--- w2d4/max_windowed_range.rb
+++ w2d4/max_windowed_range.rb
@@ -3,8 +3,8 @@ require_relative 'min_max_stack_queue.rb'
 # O(n**2)
 def naive_windowed_max_range(array, window_size)
   max_range = nil
-  array.each_cons(window_size) do |window|	# n
-    current_range = window.max - window.min	# 2n
+  array.each_cons(window_size) do |window|  # n
+    current_range = window.max - window.min # 2n
     max_range = current_range if max_range.nil?
     max_range = current_range if current_range > max_range
   end
@@ -13,16 +13,16 @@ end
 
 # O(n)
 def windowed_max_range(array, window_size)
-	stack_queue = MinMaxStackQueue.new
-	max_range = nil
+  stack_queue = MinMaxStackQueue.new
+  max_range = nil
 
-	array.each do |el|	# n
-		stack_queue.enqueue(el)
-		stack_queue.dequeue if stack_queue.size > window_size
-		current_range = stack_queue.max - stack_queue.min
-		if max_range.nil? || current_range > max_range 
-			max_range = current_range
-		end
-	end
-	max_range
+  array.each do |el|  # n
+    stack_queue.enqueue(el)
+    stack_queue.dequeue if stack_queue.size > window_size
+    current_range = stack_queue.max - stack_queue.min
+    if max_range.nil? || current_range > max_range 
+      max_range = current_range
+    end
+  end
+  max_range
 end
